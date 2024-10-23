2cb52eed5b88170d9785b1e5ce8e6f76646d5ef6
Peter Lapos <43778093+plapos@users.noreply.github.com>
2024-10-23 14:43:34 +0200

    Update question.js

--- js/question.js
+++ js/question.js
@@ -1,10 +1,10 @@
 const quiz = [
     {
-        q:'How many squares are there in the following figure?',
-        options: ['35','30', '50', '40'],
-        answer: 3,
-        img: 'img/square.png',
-        topic: 'logic'
+        q:'How do you write an if statement?',
+        options: ['if ()','als ()', 'else ()', 'else if ()'],
+        answer: 1,
+        //img: 'img/square.png',
+        topic: 'programming'
     },
     {
         q:'Which of these is an assignment operator?',
